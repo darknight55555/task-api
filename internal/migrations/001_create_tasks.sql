546250253e6652091988bac0cc375050994ab400
darknight <maksimka.pozdnyakov.98@mail.ru>
2026-07-14 16:34:16 +0700

    fix gitignore

--- internal/migrations/001_create_tasks.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE IF NOT EXISTS tasks (
-    id SERIAL PRIMARY KEY,
-    title TEXT NOT NULL,
-    done BOOLEAN NOT NULL DEFAULT false,
-    created_at TIMESTAMP NOT NULL DEFAULT NOW()
-);
